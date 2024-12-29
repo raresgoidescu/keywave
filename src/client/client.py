17b13d0046da6f5a21c98438af201e8ab2aabdeb
CristiMacovei <nicolae.macovei1412@stud.acs.upb.ro>
2024-12-29 19:46:59 +0200

    `client.py`: changed to OOP design
    Signed-off-by: CristiMacovei <nicolae.macovei1412@stud.acs.upb.ro>

--- src/client/client.py
+++ src/client/client.py
@@ -5,90 +5,136 @@ import sys
 
 from src.data_structures.events import Events
 
-def send_with_log(sck: socket.socket, msg: str, no_receive=False):
-	print(f'[INFO] Sending \'{msg}\' to server')
-	sck.send(msg.encode('utf-8'))
+class Client():
+	def __init__(self):
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.log_level = 0
+		self.logged_in = False
 
-	if no_receive:
-		return ""
 	
-	res = sck.recv(1024)
-	print(f'[INFO] Received \'{res}\' from server')
-
-	return res
+	def set_log_level(self, log_level: int):
+		self.log_level = log_level
 
 
-def send_disconnect(sck: socket.socket):
-	msg = json.dumps({
-		'event_type': Events.DISCONNECT.value
-	})
+	def connect(self, addr: tuple[str, int]):
+		self.socket.connect(addr)
 
-	res = send_with_log(sck, msg, no_receive=True)
-	return res
+		if self.log_level >= 2:
+			print(f'[INFO] Socket connected')
 
 
-def send_login(sck: socket.socket, username: str, password: str):
-	msg = json.dumps({
-		'event_type': Events.REQ_ACC_LOGIN.value,
-		'username': username,
-		'password': password
-	})
+	def __send_to_server(self, message: dict, no_receive=False):
+		json_message = None
+		try:
+			json_message = json.dumps(message)
+		except:
+			if self.log_level >= 1:
+				print(f'[ERROR] Cannot encode message :(')
+			return
+		
+		self.socket.send(json_message.encode('utf-8'))
+		if self.log_level >= 2:
+			print(f'[INFO] Sending \'{json_message}\' to server')
 
-	res = send_with_log(sck, msg)
+		if no_receive:
+			return None
 
-	return res.decode('utf-8')
+		res = self.socket.recv(1024).decode('utf-8')
+		if self.log_level >= 2:
+			print(f'[INFO] Received \'{res}\' from server')
+
+		return res
 
 
-def send_create_account(sck: socket.socket, username: str, password: str):
-	msg = json.dumps({
-		'event_type': Events.REQ_ACC_CREATE.value,
-		'username': username,
-		'password': password
-	})
+	def set_credentials(self, username: str, password: str):
+		self.username = username
+		self.password = password
 
-	res = send_with_log(sck, msg)
+	
+	def send_acc_login(self):
+		msg = {
+			'event_type': Events.REQ_ACC_LOGIN.value,
+			'username': self.username,
+			'password': self.password
+		}
 
-	return res.decode('utf-8')
+		ret = self.__send_to_server(msg)
+		self.logged_in = (ret == "Login successful")
 
-def send_message(sck: socket.socket, username: str, target: str, content: str):
-	msg = json.dumps({
-		'event_type': Events.SEND_MESSAGE.value,
-		'source': username,
-		'target': target,
-		'content': content
-	})
+		return ret
 
-	res = send_with_log(sck, msg)
-	return res.decode('utf-8')
+
+	def send_acc_create(self):
+		msg = {
+			'event_type': Events.REQ_ACC_CREATE.value,
+			'username': self.username,
+			'password': self.password
+		}
+
+		ret = self.__send_to_server(msg)
+		self.logged_in = (ret == "Login successful")
+
+		return ret
+
+	
+	def send_acc_delete(self):
+		if self.log_level >= 1:
+			print(f'[ERROR] This feature is not implemented yet')
+
+		pass
+
+	
+	def send_message(self, target: str, content: str):
+		msg = {
+			'event_type': Events.SEND_MESSAGE.value,
+			'source': self.username,
+			'target': target,
+			'content': content
+		}
+
+		return self.__send_to_server(msg)
+
+
+	def disconnect(self):
+		msg = {
+			'event_type': Events.DISCONNECT.value
+		}
+
+		_ = self.__send_to_server(msg, no_receive=True)
+
+		self.logged_in = False
+		self.socket.close()
+
+		return None
 
 PORT = 18251
 def main():
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client = Client()
 	client.connect(("0.0.0.0", PORT))
-
-	print(f'[INFO] Socket connected!')
 
 	username = input('Type username: ')
 	password = input('Type password: ')
 
-	login_res = send_login(client, username, password)
-	if login_res == "Login failed":
+	client.set_credentials(username, password)
+
+	login_res = client.send_acc_login()
+	if not client.logged_in:
 		choice = input('Login failed, would you like to create a new account with these credentials? [y/n]: ')
-		if choice == 'y':
-			send_create_account(client, username, password)
+
+		if choice.lower() == 'y':
+			client.send_acc_create()
 
 	while (True):
 		try:
 			msg = input("Type your message: ")
 			target = input("Recipient: ")
 
-			_ = send_message(client, username, target, msg)
+			_ = client.send_message(target, msg)
 
 		except KeyboardInterrupt:
 			print(f'[INFO] Disconnecting')
-			send_disconnect(client)
+			client.disconnect()
 
-			client.close()
 			sys.exit(0)
 
 
