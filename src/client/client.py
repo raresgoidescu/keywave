a501c7a86c6d25cf2bc33ec95219cbfb557959a4
CristiMacovei <nicolae.macovei1412@stud.acs.upb.ro>
2024-12-29 09:44:18 +0200

    `client.py`: functionality for login and create acc
    Signed-off-by: CristiMacovei <nicolae.macovei1412@stud.acs.upb.ro>

--- src/client/client.py
+++ src/client/client.py
@@ -11,6 +11,33 @@ def send_with_log(sck: socket.socket, msg: str):
 	res = sck.recv(1024)
 	print(f'[INFO] Received \'{res}\' from server')
 
+	return res
+
+
+def send_login(sck: socket.socket, username: str, password: str):
+	msg = json.dumps({
+		'event_type': Events.REQ_ACC_LOGIN.value,
+		'username': username,
+		'password': password
+	})
+
+	res = send_with_log(sck, msg)
+
+	return res.decode('utf-8')
+
+
+def send_create_account(sck: socket.socket, username: str, password: str):
+	msg = json.dumps({
+		'event_type': Events.REQ_ACC_CREATE.value,
+		'username': username,
+		'password': password
+	})
+
+	res = send_with_log(sck, msg)
+
+	return res.decode('utf-8')
+
+
 PORT = 18251
 def main():
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,13 +48,11 @@ def main():
 	username = input('Type username: ')
 	password = input('Type password: ')
 
-	first_message = json.dumps({
-		'event_type': Events.REQ_ACC_LOGIN.value,
-		'username': username,
-		'password': password
-	})
-
-	res = send_with_log(client, first_message)
+	login_res = send_login(client, username, password)
+	if login_res == "Login failed":
+		choice = input('Login failed, would you like to create a new account with these credentials? [y/n]: ')
+		if choice == 'y':
+			send_create_account(client, username, password)
 
 	while (True):
 		msg = input()
